8bde54cf55c6d77a8e4799ced479af3dd0d190cd
soatmuminovxurshidbek245-max <soatmuminovxurshidbek245@gmail.com>
2026-03-18 18:59:33 +0500

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('nuro-v1').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
+
